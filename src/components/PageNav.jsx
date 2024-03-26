e0718399112580dd3f166727701cfe457a8849d7
shujashah <shuja.ashraf11@gmail.com>
2024-03-27 00:18:44 +0530

    added the components and pages

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import styles from './PageNav.module.css';
+import Logo from './Logo';
 
 const PageNav = () => {
   return (
     <nav className={styles.nav}>
+      <Logo />
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
@@ -14,6 +16,11 @@ const PageNav = () => {
         </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
