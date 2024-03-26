b36a167b49e7c0c53138ecc483cf18860465c8fc
shujashah <shuja.ashraf11@gmail.com>
2024-03-26 23:14:25 +0530

    implemented routing with link and navlink

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PageNav from '../components/PageNav';
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>TourListing</h1>
+    </div>
+  );
 };
 
 export default Home;
